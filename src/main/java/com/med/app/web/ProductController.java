9b48925e7c26c81edb6900f6bc1b0f92ce64f3d3
maybe <maybeidkwhoami@outlook.fr>
2024-04-13 20:23:33 +0100

    ADDING PRODUCTS AND DIPLAYING IT, PAGING IS REMAINING

--- src/main/java/com/med/app/web/ProductController.java
+++ src/main/java/com/med/app/web/ProductController.java
@@ -6,7 +6,12 @@ import com.med.app.service.ProductManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -30,10 +35,19 @@ public class ProductController {
         return "products";
     }
 
-    @GetMapping("add_product")
+    @GetMapping("/add_product")
     public String addProduct(Model model){
         model.addAttribute("product", new Product());
         return "add_product";
     }
 
+    @PostMapping("/add_productPost")
+    public String add_product(Model model, @RequestParam(name= "productName") String productName, @RequestParam(name = "quantity") int quantity, @RequestParam(name = "unitPrice") double unitPrice){
+        Product product = new Product();
+        product.setProductName(productName);
+        product.setQuantity(quantity);
+        product.setProductUnitPrice(unitPrice);
+        productManager.addProduct(product);
+        return "redirect:products";
+    }
 }
